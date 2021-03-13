d92e29cbb5bc2f5b17f483da3b235685037041c5
jun4 <kharlep@hanmail.net>
2021-03-13 15:54:48 +0900

    Initial Commit

---
+++ ThirdChart/ThirdChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdChartApp.swift
+//  ThirdChart
+//
+//  Created by JUN? on 2021/03/13.
+//
+
+import SwiftUI
+
+@main
+struct ThirdChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
